79f37087c7959c2fccd3a8f363c117f9e11433e4
MarFilip456 <marcin.filipkowski64@gmail.com>
2022-07-09 14:19:54 +0200

    working email&password validation

--- src/components/loginRegister/DevLoginForm.js
+++ src/components/loginRegister/DevLoginForm.js
@@ -1,11 +1,69 @@
+import { useState } from "react";
 import { Fragment } from "react/cjs/react.production.min";
 import Button from "../../UI/Button";
 import classes from "./DevLoginForm.module.css";
 
 const DevLoginForm = (props) => {
+  const [passHidden, setPassHidden] = useState(true);
+
+  const [emailInput, setEmailInput] = useState("");
+  const [touchedEmail, setTouchedEmail] = useState(false);
+
+  const [passwordInput, setPasswordInput] = useState("");
+  const [touchedPassword, setTouchedPassword] = useState(false);
+
+  const incorrectEmail = !emailInput.includes("@");
+  const regex1 = new RegExp("[A-Z]");
+  const regex2 = new RegExp("[a-z]");
+  const regex3 = new RegExp("[0-9]");
+  const incorrectPassword =
+    !regex1.test(passwordInput) ||
+    !regex2.test(passwordInput) ||
+    !regex3.test(passwordInput) ||
+    passwordInput.length < 6;
+
+  const validateInputEmail = incorrectEmail && touchedEmail;
+  const validateInputPassword = incorrectPassword && touchedPassword;
+
+  const correctEmailHandler = (event) => {
+    setEmailInput(event.target.value);
+  };
+
+  const correctPasswordhandler = (event) => {
+    setPasswordInput(event.target.value);
+  };
+
+  const blurEmailHandler = () => {
+    setTouchedEmail(true);
+  };
+
+  const blurPasswordHandler = () => {
+    setTouchedPassword(true);
+  };
+
+  let passwordType = passHidden ? "password" : "text";
+
+  const togglePasswordHandler = (event) => {
+    event.preventDefault();
+    setPassHidden(!passHidden);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (incorrectEmail || incorrectPassword) {
+      console.log("dupa");
+    } else {
+      setEmailInput("");
+      setTouchedEmail(false);
+      setPasswordInput("");
+      setTouchedPassword(false);
+      setPassHidden(true);
+    }
+  };
+
   return (
     <Fragment>
-      <form className={classes.devLogin_form}>
+      <form className={classes.devLogin_form} onSubmit={formSubmitHandler}>
         <div className={classes.devLogin_form_labelContainer}>
           <label htmlFor="email">Email</label>
         </div>
@@ -17,13 +75,17 @@ const DevLoginForm = (props) => {
             />
           </div>
           <input
+            onChange={correctEmailHandler}
+            onBlur={blurEmailHandler}
             className={classes.devLogin_form_input}
             type="email"
             id="email"
             name="email"
             placeholder="E-mail"
+            value={emailInput}
           />
         </div>
+        {validateInputEmail && <p>Not valid email!</p>}
         <div className={classes.devLogin_form_labelContainer}>
           <label htmlFor="password">Password</label>
         </div>
@@ -35,19 +97,34 @@ const DevLoginForm = (props) => {
             />
           </div>
           <input
+            onBlur={blurPasswordHandler}
+            onChange={correctPasswordhandler}
             className={classes.devLogin_form_input}
-            type="password"
+            type={passwordType}
             id="password"
             name="password"
             placeholder="Password"
+            value={passwordInput}
           />
-          <button className={classes.devLogin_form_inputContainer_imgContainer}>
-            <img
-              src="https://toppng.com/uploads/preview/eye-icon-11550224069flugis6lrj.png"
-              alt="eye_icon"
-            />
+          <button
+            className={classes.devLogin_form_inputContainer_imgContainer}
+            onClick={togglePasswordHandler}
+          >
+            {passHidden && (
+              <img
+                src="https://toppng.com/uploads/preview/eye-closed-icon-11550225620ajweggaqlk.png"
+                alt="eye_icon"
+              />
+            )}
+            {!passHidden && (
+              <img
+                src="https://toppng.com/uploads/preview/eye-icon-11550224069flugis6lrj.png"
+                alt="eye_icon"
+              />
+            )}
           </button>
         </div>
+        {validateInputPassword && <p>Not valid password!</p>}
         <Button act={props.act}>{props.act}</Button>
       </form>
     </Fragment>
--- src/pages/DevNewAccPage.js
+++ src/pages/DevNewAccPage.js
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { Fragment } from "react/cjs/react.production.min";
-import Button from "../UI/Button";
+import DevFastLoginReg from "../components/loginRegister/DevFastLoginReg";
+import DevLoginForm from "../components/loginRegister/DevLoginForm";
+import SlideShow from "../components/loginRegister/SlideShow";
 
 import classes from "./DevNewAccPage.module.css";
 
@@ -10,53 +12,11 @@ const DevNewAccPage = () => {
       <div className={classes.devLogin_main}>
         <div className={classes.devLogin_main_split}>
           <h1 className={classes.devLogin_form}>Get started for free!</h1>
-          <Button styles={classes.devLogin_form_login}>
-            <img
-              className={classes.devLogin_form}
-              alt="Google logo"
-              src="https://brandlogos.net/wp-content/uploads/2015/09/google-favicon-vector.png"
-            />
-            <p className={classes.devLogin_form}>Register with Google</p>
-          </Button>
-          <Button styles={classes.devLogin_form_login}>
-            <img
-              className={classes.devLogin_form}
-              alt="GitHub logo"
-              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-            />
-            <p className={classes.devLogin_form}>Register with GitHub</p>
-          </Button>
-          <Button styles={classes.devLogin_form_login}>
-            <img
-              className={classes.devLogin_form}
-              alt="LinkedIn logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png"
-            />
-            <p className={classes.devLogin_form}>Register with LinkedIn</p>
-          </Button>
-          <Button styles={classes.devLogin_form_login}>
-            <img
-              className={classes.devLogin_form}
-              alt="Facebook logo"
-              src="https://cdn-icons-png.flaticon.com/512/124/124010.png"
-            />
-            <p className={classes.devLogin_form}>Register with Facebook</p>
-          </Button>
+          <DevFastLoginReg act="Register" />
           <div className={classes.devLogin_form_line}>
             <h3 className={classes.devLogin_form}>Or</h3>
           </div>
-          <form className={classes.devLogin_form}>
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="E-mail" />
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Password"
-            />
-            <Button className={classes.devLogin_form}>Register</Button>
-          </form>
+          <DevLoginForm act="Register" />
           <p className={classes.devLogin_form}>
             Already have an account?
             <Link to="/devlogin" className={classes.devLogin_form}>
@@ -64,13 +24,7 @@ const DevNewAccPage = () => {
             </Link>
           </p>
         </div>
-        <div className={classes.devLogin_main_split}>
-          <div>
-            <button />
-            <img alt="figure something smart" />
-            <button />
-          </div>
-        </div>
+        <SlideShow />
       </div>
     </Fragment>
   );
